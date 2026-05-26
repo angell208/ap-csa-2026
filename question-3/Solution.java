d61ca772577c033f3789dbc4db4976fa036e78c3
angell208 <angell208@nycstudents.net>
2026-05-25 20:47:44 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,1 +1,18 @@
+public int moreHistoryThanMathAbsences()
+{
+    int count = 0;
 
+    for (CourseRecord historyStudent : historyList)
+    {
+        for (CourseRecord mathStudent : mathList)
+        {
+            if (historyStudent.getStudentID().equals(mathStudent.getStudentID()) &&
+                historyStudent.getAbsences() > mathStudent.getAbsences())
+            {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
