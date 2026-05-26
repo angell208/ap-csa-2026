ae723de2ecb475ba0bbb4aa66abad7bad74f4cd3
angell208 <angell208@nycstudents.net>
2026-05-25 20:46:27 -0400

    Update Solution.java

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,1 +1,24 @@
+public class Bottle
+{
+    private double capacity;
+    private double amount;
 
+    public Bottle(double cap)
+    {
+        capacity = cap;
+        amount = cap;
+    }
+
+  
+    public double updateAmount(double removeAmt)
+    {
+        amount -= removeAmt;
+
+        if (amount < 0.25 * capacity)
+        {
+            amount = capacity;
+        }
+
+        return amount;
+    }
+}
