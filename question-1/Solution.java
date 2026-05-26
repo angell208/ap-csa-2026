540afa0d67d706c67f2d9a486aa73f36b5ce6ade
angell208 <angell208@nycstudents.net>
2026-05-25 20:44:31 -0400

    Update Solution.java

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,1 +1,40 @@
+Part (a) Constructor
+public Account(String requestedName)
+{
+    if (isAvailable(requestedName))
+    {
+        username = requestedName;
+    }
+    else
+    {
+        int num = 1;
 
+        while (!isAvailable(requestedName + num))
+        {
+            num++;
+        }
+
+        username = requestedName + num;
+    }
+}
+
+
+Part (b) getShortenedName
+public String getShortenedName()
+{
+    String shortName = "";
+
+    for (int i = 0; i < username.length(); i++)
+    {
+        if (i < username.length() - 1 && username.charAt(i + 1) == '-')
+        {
+            i++; 
+        }
+        else if (username.charAt(i) != '-')
+        {
+            shortName += username.charAt(i);
+        }
+    }
+
+    return shortName;
+}
